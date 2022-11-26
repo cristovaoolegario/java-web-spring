9d169effc1a5bbaa9b0711774e5599e1391dccf2
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-11-25 22:47:16 -0300

    chore: Add user repository

---
+++ userdept/src/main/java/com/cristovaoolegario/userdept/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cristovaoolegario.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cristovaoolegario.userdept.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
