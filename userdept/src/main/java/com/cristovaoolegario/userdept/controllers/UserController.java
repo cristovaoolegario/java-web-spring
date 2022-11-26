b899e78b18bbc6e05e590fa6fe45dc9975a5c60a
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-11-25 23:35:56 -0300

    feat: add findUserById route at [GET] /users/{id}

--- userdept/src/main/java/com/cristovaoolegario/userdept/controllers/UserController.java
+++ userdept/src/main/java/com/cristovaoolegario/userdept/controllers/UserController.java
@@ -3,9 +3,12 @@ package com.cristovaoolegario.userdept.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cristovaoolegario.userdept.entities.User;
 import com.cristovaoolegario.userdept.repositories.UserRepository;
@@ -20,6 +23,15 @@ public class UserController {
   @GetMapping
   public List<User> findAll() {
     return repository.findAll();
+  }
+
+  @GetMapping(value = "/{id}")
+  public User findById(@PathVariable Long id) {
+    var result = repository.findById(id);
+    if (result.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+    }
+    return result.get();
   }
 
 }
