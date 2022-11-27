d628b36a5f4b53fa8ef58775c2d2734deafeac8e
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-11-26 22:24:19 -0300

    feat: add insert route at [POST] /users

--- userdept/src/main/java/com/cristovaoolegario/userdept/controllers/UserController.java
+++ userdept/src/main/java/com/cristovaoolegario/userdept/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,6 +34,12 @@ public class UserController {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
     }
     return result.get();
+  }
+
+  @PostMapping
+  public User insert(@RequestBody User user) {
+    User result = repository.save(user);
+    return result;
   }
 
 }
