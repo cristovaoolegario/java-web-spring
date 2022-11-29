97c79fb0c2a099703c4bab7981a5a4319c06ff66
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-11-29 19:07:49 -0300

    chore(api): add User domain class

---
+++ api/src/main/java/com/cristovaoolegario/sampleapi/api/domain/User.java
@@ -0,0 +1,24 @@
+package com.cristovaoolegario.sampleapi.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String name;
+
+  @Column(unique = true)
+  private String email;
+  private String password;
+}
