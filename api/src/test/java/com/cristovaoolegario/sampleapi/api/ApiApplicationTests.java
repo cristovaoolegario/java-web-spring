eee0b42ecea903bcdebf8f12b9802b71dc77e3f0
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-11-28 22:55:56 -0300

    chore(api): add spring initializr project

---
+++ api/src/test/java/com/cristovaoolegario/sampleapi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristovaoolegario.sampleapi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
