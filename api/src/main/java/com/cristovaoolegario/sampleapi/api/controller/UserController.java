740bcda4a6eb164cc24a6734211f8ff7a2784e31
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-11-30 15:47:51 -0300

    fix(api): Update controller to return UserDTO instead of domain class

--- api/src/main/java/com/cristovaoolegario/sampleapi/api/controller/UserController.java
+++ api/src/main/java/com/cristovaoolegario/sampleapi/api/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cristovaoolegario.sampleapi.api.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cristovaoolegario.sampleapi.api.domain.User;
+import com.cristovaoolegario.sampleapi.api.domain.dto.UserDTO;
 import com.cristovaoolegario.sampleapi.api.services.UserService;
 
 @RestController
@@ -15,11 +16,14 @@ import com.cristovaoolegario.sampleapi.api.services.UserService;
 public class UserController {
 
   @Autowired
+  private ModelMapper mapper;
+
+  @Autowired
   private UserService service;
 
   @GetMapping(value = "/{id}")
-  public ResponseEntity<User> findById(@PathVariable Integer id) {
-    return ResponseEntity.ok().body(service.findById(id));
+  public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+    return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
   }
 
 }
---
+++ api/src/main/java/com/cristovaoolegario/sampleapi/api/domain/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.cristovaoolegario.sampleapi.api.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+  private Integer id;
+  private String name;
+  private String email;
+
+  @JsonIgnore
+  private String password;
+}
