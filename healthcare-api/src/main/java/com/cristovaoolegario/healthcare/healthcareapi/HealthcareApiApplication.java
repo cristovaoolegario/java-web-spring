8904465366c80e4711351993807bb118005a57f0
Cristóvão Olegário <cristovaoolegario@gmail.com>
2023-01-11 23:57:49 -0300

    chore(healthcare-api): add spring initializr project

---
+++ healthcare-api/src/main/java/com/cristovaoolegario/healthcare/healthcareapi/HealthcareApiApplication.java
@@ -0,0 +1,13 @@
+package com.cristovaoolegario.healthcare.healthcareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareApiApplication.class, args);
+	}
+
+}
---
+++ healthcare-api/src/test/java/com/cristovaoolegario/healthcare/healthcareapi/HealthcareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristovaoolegario.healthcare.healthcareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
